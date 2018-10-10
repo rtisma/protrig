09fb3ed8922ac2824ddb4cd54814b535a5ec11d0
rtisma <rtisma@gmail.com>
2018-10-10 13:51:08 -0400

    updated port

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 version=latest
+port=8888
 docker pull rtisma1/protrig:${version}
-docker run --rm -it rtisma1/protrig:${version}  --spring.profiles.active=dev
+docker run --rm -p ${port}:8080 -it rtisma1/protrig:${version}  --spring.profiles.active=dev
