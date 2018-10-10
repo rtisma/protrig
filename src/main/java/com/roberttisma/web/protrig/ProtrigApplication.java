4137f249a4c55c24862e75b2c87d4e3ed09fa70d
rtisma <rtisma@gmail.com>
2018-10-10 09:06:26 -0400

    updated

---
+++ src/main/java/com/roberttisma/web/protrig/ProtrigApplication.java
@@ -0,0 +1,12 @@
+package com.roberttisma.web.protrig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtrigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtrigApplication.class, args);
+	}
+}
